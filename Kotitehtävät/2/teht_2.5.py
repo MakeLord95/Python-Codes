23206820d557ebcfb358eaabec80aa465c221d6b
Make <juhani.markus@gmail.com>
2022-08-23 13:48:39 +0300

    tehtävän 2.5 kommenttien editointi

--- Kotitehtävät/2/teht_2.5.py
+++ Kotitehtävät/2/teht_2.5.py
@@ -9,18 +9,23 @@ luoti = float(input("Anna luodit: "))
 # print(f"Luodit: {luoti}")
 
 naula2 = (leiviska * 20) + naula
+# Munnetaan leiviskät nauloiksi ja lisätään päälle käyttäjän syöttämät naulat
 # print(f"Naulat 2 : {naula2}")
 
 luoti2 = (naula2 * 32) + luoti
+# Muunnetaan naulat luodeiksi ja lisätän päälle käyttäjän syöttämät luodit
 # print(f"Luodit 2 : {luoti2}")
 
 gramma = (luoti2 * 13.3)
+# Muunnetaan luodit grammoiksi
 # print(f"Grammaa: {gramma}")
 
 kilogramma = gramma // 1000
+# Jaetaan grammat tuhannella käyttämällä // (Unohtaa kaiken mitä jäisi jäljelle jakolaskusta (esim 23.876 = 23.0))
+
 gramma2 = gramma % 1000
-# print(f"Kilogrammaa: {kilogramma:.0f}")
-# print(f"Grammaa: {gramma2:.2f}")
+# Jaetaan grammat tuhannella käyttämällä % (Unohtaa kokoluvun kokonaan ja käyttää pelkästään numeroita jotka jäävät
+# desimaalin taakse (esim 23.876 = 876))
 
 print(f"Massa nykymittojen mukaan: ")
 print(f"{kilogramma:.0f} kilogrammaa ja {gramma2:.2f} grammaa.")
