e6a6288320382b95201f13c464496b1e1c918953
Make <juhani.markus@gmail.com>
2022-08-25 19:44:17 +0300

    -

--- Kotitehtävät/4/teht_4.3.py
+++ Kotitehtävät/4/teht_4.3.py
@@ -1,17 +1,21 @@
-OHJELMA KESKEN!
-EI OLE VIELÄ VALMIS!!!
+# OHJELMA KESKEN!
+# EI OLE VIELÄ VALMIS!!!
 
 nbr = input("Syötä luku: ")
 nbr_big = nbr
 nbr_smol = nbr
-while nbr != '':
-    if nbr_big < nbr:
-        nbr_big = nbr
 
-    if nbr_smol > nbr and nbr_smol < nbr_big:
-        nbr_smol = nbr
+while True:
+    if nbr != '':
+        if nbr > nbr_big:
+            nbr_big = nbr
 
-    nbr = input("Syötä luku: ")
+        if nbr < nbr_smol:
+            nbr_smol = nbr
 
-print(nbr_big)
-print(nbr_smol)
+        nbr = None
+        nbr = input("Syötä luku: ")
+    else:
+        print(f"Big: {nbr_big}")
+        print(f"Smol: {nbr_smol}")
+        break
