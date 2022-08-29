e135396828ef7e8615d8fccc52ebb32264f7778e
Make <juhani.markus@gmail.com>
2022-08-29 15:42:49 +0300

    Moduuli 5 kotitehtävä 5.2

--- Kotitehtävät/5/teht_5.2.py
+++ Kotitehtävät/5/teht_5.2.py
@@ -1,1 +1,17 @@
-# WIP
+num = int
+list = []
+res = None
+
+while num != "":
+    num = str(input("Anna luku: "))
+
+    if num != "":
+        list.append(num)
+        res = [eval(i) for i in list]
+
+    if input == "":
+        break
+
+res.sort(reverse=True)
+fi = res[0:5]
+print(f"Suurimmat luvut ovat: {fi}")
