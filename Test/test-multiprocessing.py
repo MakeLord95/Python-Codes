4faca79d2bde974ae1584f0cc76e6b6c586377d7
Make <juhani.markus@gmail.com>
2022-08-29 17:41:42 +0300

    test-multiprocessing upload

---
+++ Test/test-multiprocessing.py
@@ -0,0 +1,6 @@
+import multiprocessing
+print("Number of CPU cores: ", multiprocessing.cpu_count())
+
+# Alternative way of doing this:
+# from multiprocessing import cpu_count
+# print("Number of CPU cores: ", cpu_count())
