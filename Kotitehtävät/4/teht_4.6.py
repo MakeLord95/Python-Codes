f293de1067d12fadbbe053f84b23c73354de4b4a
Make <juhani.markus@gmail.com>
2022-09-01 15:16:04 +0300

    Moduuli 4, tehtävä 4.6 editointi

--- Kotitehtävät/4/teht_4.6.py
+++ Kotitehtävät/4/teht_4.6.py
@@ -7,8 +7,9 @@ circle_points = 0
 square_points = 0
 pi = 0
 
-range_value = math.floor(math.pow(user, 2))
-for i in range(range_value):
+user_int = int(math.pow(user, 2))
+
+for i in range(user_int):
 
     random_x = random.uniform(-1, 1)
     random_y = random.uniform(-1, 1)
