afcba31ead5626adfbf11ae21a5746c1afb38fcb
Make <juhani.markus@gmail.com>
2022-09-01 15:12:40 +0300

    Moduuli 4, tehtävä 4.6 editointi

--- Kotitehtävät/4/teht_4.6.py
+++ Kotitehtävät/4/teht_4.6.py
@@ -1,17 +1,19 @@
 import random
+import math
 
-INTERVAL = int(input("Syötä pisteiden määrä: "))
+user = int(input("Syötä pisteiden määrä: "))
 
 circle_points = 0
 square_points = 0
 pi = 0
 
-for i in range(INTERVAL ** 2):
+range_value = math.floor(math.pow(user, 2))
+for i in range(range_value):
 
-    rand_x = random.uniform(-1, 1)
-    rand_y = random.uniform(-1, 1)
+    random_x = random.uniform(-1, 1)
+    random_y = random.uniform(-1, 1)
 
-    origin_dist = rand_x ** 2 + rand_y ** 2
+    origin_dist = random_x ** 2 + random_y ** 2
 
     if origin_dist <= 1:
         circle_points += 1
