752ea96f2818632abaa1d70d4afbd9ecf0f93b0f
Make <juhani.markus@gmail.com>
2022-09-02 11:15:09 +0300

    Moduuli 7, teht_7.1 WIP

---
+++ Kotitehtävät/7/teht_7.1.py
@@ -0,0 +1,22 @@
+seasons = ("Kevät", "Kesä", "Syksy", "Talvi")
+
+print(seasons)
+user_input = int(input(": "))
+if user_input == 1 or user_input == 2 or user_input == 3:
+    season =
+
+res = seasons[user_input - 1]
+print(res)
+
+
+
+# Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron,
+# jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan
+# (kevät, kesä, syksy, talvi).
+#
+# Tallenna ohjelmassasi kuukausia
+# vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
+
+# Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi
+# siten, että joulukuu on ensimmäinen talvikuukausi.
+
