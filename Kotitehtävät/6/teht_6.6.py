c6bc0c69c8b0420cc643a57a43caa9ce91fa0b0e
Make <juhani.markus@gmail.com>
2022-09-08 14:05:24 +0300

    Editointi

--- Kotitehtävät/6/teht_6.6.py
+++ Kotitehtävät/6/teht_6.6.py
@@ -4,14 +4,15 @@ import math
 def pizza_calculator(pizza_dia_cm, pizza_price):
     pizza_area_cm2 = math.pi / 4 * pizza_dia_cm ** 2
     pizza_area_m2 = pizza_area_cm2 * 0.01 * 0.01
-    pizza_price_m2 = pizza_price / pizza_area_m2
+    pizza_price_per_m2 = pizza_price / pizza_area_m2
 
-    return pizza_price_m2
+    return pizza_price_per_m2
 
 
 if True:
     pizza_1_dia_cm = float(input("1. pizzan halkaisia (cm): "))
     pizza_1_price = float(input("1. pizzan hinta: "))
+
     pizza_2_dia_cm = float(input("2. pizzan halkaisia (cm) : "))
     pizza_2_price = float(input("2. pizzan hinta: "))
 
