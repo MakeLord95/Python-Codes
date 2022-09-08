a95fa9e8b6ddd127cf52b9d9365cd5e6709abc5d
Make <juhani.markus@gmail.com>
2022-09-08 14:12:48 +0300

    Editointi

--- Kotitehtävät/6/teht_6.6.py
+++ Kotitehtävät/6/teht_6.6.py
@@ -2,7 +2,7 @@ import math
 
 
 def pizza_calculator(pizza_dia_cm, pizza_price):
-    pizza_area_cm2 = math.pi / 4 * pizza_dia_cm ** 2
+    pizza_area_cm2 = math.pi / 4 * (pizza_dia_cm ** 2)
     pizza_area_m2 = pizza_area_cm2 * 0.01 * 0.01
     pizza_price_per_m2 = pizza_price / pizza_area_m2
 
@@ -22,5 +22,8 @@ if True:
     if pizza_1 > pizza_2:
         print("Toinen pizza on neliöhinnaltaan parempi!")
 
-    else:
+    elif pizza_2 > pizza_1:
         print("Ensimmäinen pizza on neliöhinnaltaan parempi!")
+
+    else:
+        print("Pizzat ovat neliöhinnaltaan samanlaisia")
