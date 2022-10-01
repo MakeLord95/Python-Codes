654099683ea1b338b8c79fd8858ebd4d7e2381d0
Make <juhani.markus@gmail.com>
2022-10-01 21:41:20 +0300

    :D

---
+++ Test/test-playsound.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+# Use playsound 1.2.2
+
+while True:
+    playsound(r"C:\Users\Make\Downloads\Why-Are-You-Gay.wav")
+    playsound(r"C:\Users\Make\Downloads\Ali-A.wav")
+    playsound(r"C:\Users\Make\Downloads\Coffin-Dance.wav")
+    playsound(r"C:\Users\Make\Downloads\Cyka-Blyat.wav")
