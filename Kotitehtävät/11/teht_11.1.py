3d81e904f5663adf6089dd0409f83a23df2b104a
Make <juhani.markus@gmail.com>
2022-10-26 23:24:35 +0300

    Moduuli 11 teht_11.1 sekä teht_11.2 tehty

---
+++ Kotitehtävät/11/teht_11.1.py
@@ -0,0 +1,38 @@
+class Julkaisu:
+    def __init__(self, name):
+        self.nimi = name
+
+
+    def tulosta_tiedot(self):
+        print(f"\nJulkaisun nimi: {self.nimi}")
+
+
+class Kirja(Julkaisu):
+    def __init__(self, name, writer, page_count):
+        super().__init__(name)
+        self.writer = writer
+        self.page_count = page_count
+
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Kirjan kirjoittaja: {self.writer}")
+        print(f"Kirjan sivumäärä: {self.page_count}")
+
+
+class Lehti(Julkaisu):
+    def __init__(self, name, editor_in_chief):
+        super().__init__(name)
+        self.editor_in_chief = editor_in_chief
+
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Lehden päätoimittaja: {self.editor_in_chief}")
+
+
+if __name__ == '__main__':
+    julkaisut = [Kirja("Hytti n:o 6", "Rosa Liksom", 200), Lehti("Aku Ankka", "Aki Hyyppä")]
+
+    for i in julkaisut:
+        i.tulosta_tiedot()
---
+++ Kotitehtävät/11/teht_11.2.py
@@ -0,0 +1,68 @@
+import random
+
+
+class Auto:
+    def __init__(self, licence, max_speed):
+        self.licence = licence
+        self.max_speed = max_speed
+        self.speed = 0
+        self.traveled = 0
+
+
+    def tulosta_tiedot(self):
+        print(f"Auton {self.licence}")
+        print(f"Matkamittarin lukema: {self.traveled}km\n")
+
+
+    def accelerate(self, speed_change):
+        if 0 < self.speed + speed_change < self.max_speed:
+            self.speed = self.speed + speed_change
+
+        elif self.speed + speed_change <= 0:
+            self.speed = 0
+
+        elif self.speed + speed_change > self.max_speed:
+            self.speed = self.max_speed
+
+
+    def travel(self, aika):
+        if aika < 0:
+            self.traveled = self.traveled
+
+        elif aika > 0:
+            self.traveled = self.traveled + (self.speed * aika)
+
+
+class Sahkoauto(Auto):
+    def __init__(self, licence, max_speed, akkukapasitetti):
+        super().__init__(licence, max_speed)
+        self.akkukapasiteetti = akkukapasitetti
+
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+
+
+class Polttomoottoriauto(Auto):
+    def __init__(self, licence, max_speed, bensatankki_litra):
+        super().__init__(licence, max_speed)
+        self.bensatankki_litra = bensatankki_litra
+
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+
+
+
+if __name__ == '__main__':
+    electric_car = Sahkoauto("ABC-15", 180, 52.5)
+    petrol_car = Polttomoottoriauto("ACD-123", 165, 32.3)
+
+    electric_car.accelerate(random.randint(1, 180))
+    electric_car.travel(3)
+
+    petrol_car.accelerate(random.randint(1, 165))
+    petrol_car.travel(3)
+
+    electric_car.tulosta_tiedot()
+    petrol_car.tulosta_tiedot()
